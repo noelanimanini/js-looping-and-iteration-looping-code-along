ac3109b203dbe90098b9083a78e47594468b0fb4
crystal <noelani26@crystals-MacBook-Pro.local>
2021-01-11 19:44:48 -0800

    heres the flavor flav;

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+function writeCards (namesArray, event) {
+    let thankYouCards = []
+        for ( let i = 0; i < namesArray.length; i++) {
+            thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`)
+        }
+        return thankYouCards
+}
+// function writeCards( namesArray, event ) {
+//     let thankYouCards = []
+//     for ( let i = 0; i < namesArray.length; i++ ) {
+//       thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
+//     }
+//     return thankYouCards
+//   }
+ 
+function countDown( number) {
+    while (number > 0) {
+        console.log(number);
+        number -= 1
+    }
+    console.log(number);
+}
+
